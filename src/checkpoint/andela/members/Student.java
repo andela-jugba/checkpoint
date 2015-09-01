8dc67ee198bc7d4c469771028caa86e0547ca300
Joshua Ugba <justjosh001@gmail.com>
2015-09-01 17:29:24 +0100

    Add student class

--- src/checkpoint/andela/members/Student.java
+++ src/checkpoint/andela/members/Student.java
@@ -5,6 +5,7 @@ import checkpoint.andela.main.Member;
 public class Student extends Member {
 	private String StudentNumber;
 	private String mClass;
+	
 	public Student() {
 		super();
 		// TODO Auto-generated constructor stub
@@ -12,6 +13,25 @@ public class Student extends Member {
 	public Student(String fullName, char g, String dateOfbirth, String phoneNumber) {
 		super(fullName, g, dateOfbirth, phoneNumber);
 		// TODO Auto-generated constructor stub
+	}
+	
+	// return the student number
+	public String getStudentNumber() {
+		return StudentNumber;
+	}
+	
+	//set the student number
+	public void setStudentNumber(String studentNumber) {
+		StudentNumber = studentNumber;
+	}
+	
+	// return the class of student
+	public String getmClass() {
+		return mClass;
+	}
+	// set the class of the student
+	public void setmClass(String mClass) {
+		this.mClass = mClass;
 	}
 	
 }
