8b067d63447ed84cfdaeda7990317912073234ca
Joshua Ugba <justjosh001@gmail.com>
2015-09-01 12:47:39 +0100

    Test member class exists

--- src/test/checkpoint/andela/main/MemberTest.java
+++ src/test/checkpoint/andela/main/MemberTest.java
@@ -4,11 +4,14 @@ import static org.junit.Assert.*;
 
 import org.junit.Test;
 
+import checkpoint.andela.main.Member;
+
 public class MemberTest {
 
 	@Test
-	public void test() {
-		fail("Not yet implemented");
+	public void testMember(){
+		Member member = new Member("James Harden",'M', "12/3/1988", "23243545");
+		assertNotNull(member);
 	}
 
 }
