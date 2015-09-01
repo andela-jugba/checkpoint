33605cb3d3f46c312e60667381bae396800c1ea1
Joshua Ugba <justjosh001@gmail.com>
2015-09-01 11:11:52 +0100

    Initial Commit, setup project root

---
+++ src/test/checkpoint/andela/main/MemberTest.java
@@ -0,0 +1,14 @@
+package test.checkpoint.andela.main;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class MemberTest {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
