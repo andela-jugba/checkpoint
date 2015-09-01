5cd454b2a7152e7d196c7ce11d946372f41c29a3
Joshua Ugba <justjosh001@gmail.com>
2015-09-02 00:42:31 +0100

    Update Staff Class

--- src/checkpoint/andela/members/Staff.java
+++ src/checkpoint/andela/members/Staff.java
@@ -3,14 +3,33 @@ package checkpoint.andela.members;
 import checkpoint.andela.main.Member;
 
 public class Staff extends Member {
+	
+	private String staffNumber;
+	private Double netPay;
 
 	public Staff(String fullName, char g, String dateOfbirth, String phoneNumber) {
 		super(fullName, g, dateOfbirth, phoneNumber);
 		// TODO Auto-generated constructor stub
 	}
-
 	public Staff() {
 		// TODO Auto-generated constructor stub
+	}
+
+	public String getStaffNumber() {
+		return staffNumber;
+	}
+
+	public void setStaffNumber(String staffNumber) {
+		this.staffNumber = staffNumber;
+	}
+
+	public Double getNetPay() {
+		return netPay;
+	}
+
+	public void setNetPay(Double netPay) {
+		if(netPay > 0)this.netPay = netPay;
+		else throw new IllegalArgumentException("Netpay must be greater than Zero");
 	}
 
 }
--- src/test/checkpoint/andela/members/StaffTest.java
+++ src/test/checkpoint/andela/members/StaffTest.java
@@ -4,11 +4,14 @@ import static org.junit.Assert.*;
 
 import org.junit.Test;
 
+import checkpoint.andela.members.Staff;
+
 public class StaffTest {
 
 	@Test
-	public void test() {
-		fail("Not yet implemented");
+	public void testStaff() {
+		Staff sf = new Staff();
+		assertNotNull("Should not be null", sf);
 	}
-
+	
 }
