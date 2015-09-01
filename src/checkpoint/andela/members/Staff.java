ae904059adc28e47d7de7b6dd2629313d6f0bd35
Joshua Ugba <justjosh001@gmail.com>
2015-09-01 17:32:21 +0100

    Add Staff class

---
+++ src/checkpoint/andela/members/Staff.java
@@ -0,0 +1,16 @@
+package checkpoint.andela.members;
+
+import checkpoint.andela.main.Member;
+
+public class Staff extends Member {
+
+	public Staff(String fullName, char g, String dateOfbirth, String phoneNumber) {
+		super(fullName, g, dateOfbirth, phoneNumber);
+		// TODO Auto-generated constructor stub
+	}
+
+	public Staff() {
+		// TODO Auto-generated constructor stub
+	}
+
+}
